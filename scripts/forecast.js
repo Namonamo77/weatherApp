89f7b362d0b473210afdb97c7d8d4e0f05b7aa54
Namonamo <kactusworld.design@gmail.com>
2022-09-03 09:53:30 +0200

    Added HTTPS
    Corrected http to https

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,8 @@
 class Forecast {
     constructor(){
         this.key = "LC1FAAo21SOZ2QB7NPRHXe6DNkdfcCaT"
-        this.weatherURI = "http://dataservice.accuweather.com/currentconditions/v1/"
-        this.cityURI = "http://dataservice.accuweather.com/locations/v1/cities/search"
+        this.weatherURI = "https://dataservice.accuweather.com/currentconditions/v1/"
+        this.cityURI = "https://dataservice.accuweather.com/locations/v1/cities/search"
     }
     async updateCity(city){
         
